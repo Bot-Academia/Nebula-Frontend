ff6af26f3980603fcdbf46c964a65e93e69b9ea0
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-11-24 00:45:32 +0530

    added signup
    Signed-off-by: Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>

---
+++ pages/signup.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="jumbotron container">
+    <h1><b>Sign Up</b></h1>
+    <br>
+    <form class="container">
+      <b-form-input placeholder="Name" :type="'text'" :state="null" required />
+      <br>
+      <b-form-input placeholder="E-Mail" :type="'email'" :state="null" required />
+      <br>
+      <b-form-input placeholder="Password" :type="'password'" :state="null" required />
+      <br>
+      <b-form-input placeholder="Confirm Password" :type="'password'" :state="null" required />
+      <br>
+      <b-form-input placeholder="Entry No" :type="'text'" :state="null" required />
+      <br>
+    </form>
+    <button class="btn btn-success">
+      <i class="fa fa-user-plus" aria-hidden="true" /> Sign-Up
+    </button>
+    <br><br>
+    <br><br>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.jumbotron{
+	background: rgba(0, 0, 0, 0.75);
+    width: 35%;
+    height: 50%;
+    margin-top: 3%;
+    color: white;
+}
+.jumbotron button{
+    float: right;
+}
+@media (max-width: 900px){
+    .jumbotron{
+        width: 50%;
+    }
+}
+@media (max-width: 400px){
+    .jumbotron{
+        width: 70%;
+    }
+}
+</style>
